41c28d948ba243afd890251a72c67736514b8d14
Sanilal P Sathyan <sanilal@gmail.com>
2024-05-02 17:35:35 +0400

    header component added

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <div className="header-contents">
+            <h2>Indulge Your Taste Buds: Order Now!”</h2>
+            <p>Whether it’s a cheesy pizza, a juicy burger, or a refreshing salad, our menu has something to satisfy every craving. Don’t wait—place your order and treat yourself to a delightful culinary experience! Bon appétit!  </p>
+            <button>View Menu</button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
