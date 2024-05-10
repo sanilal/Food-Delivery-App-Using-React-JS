c68af9ff7e17261a1b74cb678ef8e991ff2d8e2c
Sanilal P Sathyan <sanilal@gmail.com>
2024-05-10 18:03:25 +0400

    Mongoose connection added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors"
+import { connectDB } from "./config/db.js";
 
 
 
@@ -11,10 +12,16 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+// db connection
+
+connectDB();
+
 app.get("/", (req,res)=>{
     res.send("API Working")
 })
 
-spp.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log(`Server running on http://localhost:${port}`)
 })
+
+//mongodb+srv://tomatouser:oi6ErIabMCAUC2RC@cluster0.iprf463.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
